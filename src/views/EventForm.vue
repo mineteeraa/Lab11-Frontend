d9904d3331b3707f464062dc23e5d04da94c1c3f
mineteeraa <patteera_mine@hotmail.com>
2021-09-24 22:14:47 +0700

    [2.10] Update code to show the image in event details

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -46,7 +46,8 @@ export default {
         title: '',
         description: '',
         location: '',
-        organizer: { id: '', name: '' }
+        organizer: { id: '', name: '' },
+        imageUrls: []
       },
       files: []
     }
@@ -58,26 +59,24 @@ export default {
           return EventService.uploadFile(file)
         })
       ).then((response) => {
-        console.log(response)
-        console.log('finish upload file')
-      })
-
-      EventService.saveEvent(this.event)
-        .then((response) => {
-          console.log(response)
-          this.$router.push({
-            name: 'EventLayout',
-            params: { id: response.data.id }
+        this.event.imageUrls = response.map((r) => r.data)
+        EventService.saveEvent(this.event)
+          .then((response) => {
+            console.log(response)
+            this.$router.push({
+              name: 'EventLayout',
+              params: { id: response.data.id }
+            })
+            this.GStore.flashMessage =
+              'You are successfully add a new event for ' + response.data.title
+            setTimeout(() => {
+              this.GStore.flashMessage = ''
+            }, 3000)
           })
-          this.GStore.flashMessage =
-            'You are successfully add a new event for ' + response.data.title
-          setTimeout(() => {
-            this.GStore.flashMessage = ''
-          }, 3000)
-        })
-        .catch(() => {
-          this.$router.push('NetworkError')
-        })
+          .catch(() => {
+            this.$router.push('NetworkError')
+          })
+      })
     },
     handleImages(files) {
       this.files = files
